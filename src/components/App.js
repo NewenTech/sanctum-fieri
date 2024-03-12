1883639917e9afe119365fa5c910feb60c77e7b0
SergioAlballay <sergio.alballay@pm.me>
2024-03-12 14:07:13 +1100

    add center and nav files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from './Banner';
+import Nav from './Nav';
+import Center from './Center';
 import MyCarousel from './Carousel';
 import Footer from './Footer';
 function App() {
 	return (
 		<div>
 			<Banner />
+			<Nav />
 			<MyCarousel />
+			<Center />
 			<Footer />
 		</div>
 	);
