a0f5e512e75366c42664ebf9ef8aa55112bba35b
SergioAlballay <sergio.alballay@pm.me>
2024-03-12 15:57:50 +1100

    WIP react router nav bar

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Center from '../components/Center';
+import MyCarousel from '../components/Carousel';
+
+function Home() {
+	return (
+		<div>
+			<MyCarousel />
+			<Center />
+		</div>
+	);
+}
+
+export default Home;
