daa7931cf46cdeaef1f445cd13316d3aeef224db
SergioAlballay <sergio.alballay@pm.me>
2024-03-13 15:15:20 +1100

    WIP services cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import classes from './Card.module.css'; // Import the CSS module for styling
 
 function Card() {
-	return <h1>Card</h1>;
+	return (
+		<div className={classes.cardContainer}>
+			<div className={classes.card}>
+				<div className={classes.cardImgContainer}>
+					<img
+						src='placeholder1.jpg'
+						alt='Image 1'
+						className={classes.cardImg}
+					/>
+					<div className={classes.overlay}>
+						<div className={classes.text}>Text on hover</div>
+					</div>
+				</div>
+				<div className={classes.cardContent}>
+					<h3>Title 1</h3>
+					<p>Description 1</p>
+				</div>
+			</div>
+			<div className={classes.card}>
+				<div className={classes.cardImgContainer}>
+					<img
+						src='placeholder2.jpg'
+						alt='Image 2'
+						className={classes.cardImg}
+					/>
+					<div className={classes.overlay}>
+						<div className={classes.text}>Text on hover</div>
+					</div>
+				</div>
+				<div className={classes.cardContent}>
+					<h3>Title 2</h3>
+					<p>Description 2</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Card;
