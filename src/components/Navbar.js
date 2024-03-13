1926de622775bde07bac796d1fbe1165d7df794b
SergioAlballay <sergio.alballay@pm.me>
2024-03-13 13:27:57 +1100

    center nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,32 @@
-// Navbar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
 	return (
-		<nav className='navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-center'>
-			<div className='container'>
-				<Link className='navbar-brand' to='/'>
-					Navbar
-				</Link>
+		<nav
+			className='navbar navbar-expand-lg bg-body-tertiary rounded fs-3'
+			aria-label='navbar'
+		>
+			<div className='container-fluid'>
 				<button
 					className='navbar-toggler'
 					type='button'
 					data-bs-toggle='collapse'
-					data-bs-target='#navbarNav'
-					aria-controls='navbarNav'
+					data-bs-target='#navbarsExample10'
+					aria-controls='navbarsExample10'
 					aria-expanded='false'
 					aria-label='Toggle navigation'
 				>
 					<span className='navbar-toggler-icon'></span>
 				</button>
-				<div className='collapse navbar-collapse' id='navbarNav'>
+
+				<div
+					className='collapse navbar-collapse justify-content-md-center'
+					id='navbarsExample10'
+				>
 					<ul className='navbar-nav'>
 						<li className='nav-item'>
-							<Link className='nav-link' to='/'>
+							<Link className='nav-link active' aria-current='page' to='/'>
 								Home
 							</Link>
 						</li>
