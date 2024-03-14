44babe2eba44a87f10820ec86b96a3ac369595d6
SergioAlballay <sergio.alballay@pm.me>
2024-03-14 16:04:28 +1100

    services offer pictures and data

---
+++ src/data/offerData.js
@@ -0,0 +1,29 @@
+import meditate from '../images/meditate.webp';
+import meeting from '../images/meeting.webp';
+import students from '../images/students.webp';
+
+const offerData = [
+	{
+		id: 0,
+		imageSrc: meditate,
+		title: '1:1 Counselling',
+		description:
+			'Looking for a Counsellor who understands your Catholic Faith? Sanctum Fieri only provides Counsellors who are practising Catholics. We hope to meet you online or via phone for a quick chat to see if we are a good fit.',
+	},
+	{
+		id: 1,
+		imageSrc: meeting,
+		title: 'Connecting Catholic Singles',
+		description:
+			'Sanctum Fieri takes pride in being the place where Catholic singles can come together, from around Australia, to learn, practise, connect and grow in holiness, in preparation for their vocation.',
+	},
+	{
+		id: 2,
+		imageSrc: students,
+		title: 'Online Courses',
+		description:
+			'You will grow in virtue, through learning more about yourself and will be encouraged to strive for holiness through practical exercises and reflections. You have the opportunity to raise questions and share in discussion with other like-minded singles. ',
+	},
+];
+
+export default offerData;
