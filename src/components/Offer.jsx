50e942ad4c9b13a9aff96f14b161cdc80c06ab6d
SergioAlballay <sergio.alballay@pm.me>
2024-03-19 16:49:54 +1100

    WIP Offer grid

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styles from './Offer.module.css';
+import meeting from '../images/meeting.webp';
+import meditate from '../images/meditate.webp';
+import students from '../images/students.webp';
 
 function Offer() {
 	return (
 		<div>
 			<h1>What we offer</h1>
+			<div className={styles.grid_container}>
+				<div className={styles.purple}> black </div>
+				<div className={''}> white </div>
+				<div className={styles.purple}> black </div>
+				<div className={styles.white}> white </div>
+				<div className={styles.purple}> black </div>
+				<div className={styles.white}> white </div>
+			</div>
 		</div>
 	);
 }
